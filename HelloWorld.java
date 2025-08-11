ce7a63a24b546956d51619481f5754a9458a1e76
root <root@localhost.localdomain>
2025-08-11 12:37:25 +0530

    UpdatedV2 HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins!");
 	System.out.println("I have Configured Jenkin Automation!");
-	System.out.println("Final Data");
+	System.out.println("Rajjan You have Successfully made jenkin Automation build");
     }
 }
 
